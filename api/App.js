80a8dce9ff16f0bb3e9fabce63a5a53cb98aa340
awcl <113561942+awcl@users.noreply.github.com>
2022-12-05 10:20:13 -0700

    init

---
+++ api/App.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
+app.use(express.json()).use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).send(movies)
+});
+
+app.get('/:query', (req, res) => {
+  res.status(200).send(`Queried ${req.params.query}`)
+})
+
+app.listen(3001, () => {console.log(`I'm doing the thing on 3001`)});
